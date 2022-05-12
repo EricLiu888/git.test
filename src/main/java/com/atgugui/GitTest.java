053a022a4bed5ea71c5b7b1453482d5a4f3db2c0
eric <eric@atguigu.com>
2022-05-11 21:05:55 -0700

    push commit

--- src/main/java/com/atgugui/GitTest.java
+++ src/main/java/com/atgugui/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("git2");
         System.out.println("hot-fix");
         System.out.println("hot-fix1");
+        System.out.println("push test");
 
 
 
